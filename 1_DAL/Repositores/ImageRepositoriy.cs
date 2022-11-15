2487cc8b30dfc77e5d443296b4e8e1dbfe6ec219
Nam Hai <hainnph27584@fpt.edu.vn>
2022-11-15 15:10:20 +0700

    them repos image

---
+++ 1_DAL/Repositores/ImageRepositoriy.cs
@@ -0,0 +1,71 @@
+﻿using _1_DAL.Context;
+using _1_DAL.IRepositories;
+using _1_DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_DAL.Repositores
+{
+    public class ImageRepositoriy : IImageRepositoriy
+    {
+        ManagerContext _DBcontext;
+        public ImageRepositoriy()
+        {
+            _DBcontext = new ManagerContext();
+        }
+        public bool add(Anh image)
+        {
+            try
+            {
+                _DBcontext.Anhs.Add(image);
+                _DBcontext.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public List<Anh> getAll()
+        {
+            return _DBcontext.Anhs.ToList();
+        }
+
+        public bool remove(Anh image)
+        {
+             
+            try
+            {
+                var x = _DBcontext.Anhs.FirstOrDefault(p=>p.ID==image.ID);
+                _DBcontext.Anhs.Remove(x);
+                _DBcontext.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public bool update(Anh image)
+        {
+            try
+            {
+                _DBcontext.Anhs.Update(image);
+                _DBcontext.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
--- 2_BUS/ViewModels/ChucVuViewModels.cs
+++ 2_BUS/ViewModels/ChucVuViewModels.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace _2_BUS.ViewModels
 {
-    public class NhanVienViewModels
+    public class ChucVuViewModels
     {
     }
 }
