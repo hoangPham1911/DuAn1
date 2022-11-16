ff3dfdac68cbe17b70a4dcb5f10874d96e6cd0a0
hello <huyenkttph27572@fpt.edu.vn>
2022-11-16 22:03:36 +0700

    sua frm

--- 3_PL/View/Frm_HoaDon.Designer.cs
+++ 3_PL/View/Frm_HoaDon.Designer.cs
@@ -77,98 +77,63 @@
             // 
             this.textBox6.Location = new System.Drawing.Point(381, 365);
             this.textBox6.Name = "textBox6";
-
-            this.textBox6.Size = new System.Drawing.Size(330, 31);
-
-            this.textBox6.Size = new System.Drawing.Size(221, 23);
-
+            this.textBox6.Size = new System.Drawing.Size(273, 31);
             this.textBox6.TabIndex = 44;
             // 
             // textBox5
             // 
             this.textBox5.Location = new System.Drawing.Point(381, 297);
             this.textBox5.Name = "textBox5";
-
-            this.textBox5.Size = new System.Drawing.Size(330, 31);
-
-            this.textBox5.Size = new System.Drawing.Size(221, 23);
-
+            this.textBox5.Size = new System.Drawing.Size(273, 31);
             this.textBox5.TabIndex = 43;
             // 
             // textBox3
             // 
             this.textBox3.Location = new System.Drawing.Point(381, 110);
             this.textBox3.Name = "textBox3";
-
-            this.textBox3.Size = new System.Drawing.Size(330, 31);
-
-            this.textBox3.Size = new System.Drawing.Size(221, 23);
-
+            this.textBox3.Size = new System.Drawing.Size(273, 31);
             this.textBox3.TabIndex = 42;
             // 
             // textBox7
             // 
             this.textBox7.Location = new System.Drawing.Point(381, 235);
             this.textBox7.Name = "textBox7";
-
-            this.textBox7.Size = new System.Drawing.Size(330, 31);
-
-            this.textBox7.Size = new System.Drawing.Size(221, 23);
-
+            this.textBox7.Size = new System.Drawing.Size(273, 31);
             this.textBox7.TabIndex = 40;
             // 
             // textBox2
             // 
             this.textBox2.Location = new System.Drawing.Point(381, 175);
             this.textBox2.Name = "textBox2";
-
-            this.textBox2.Size = new System.Drawing.Size(330, 31);
-
-            this.textBox2.Size = new System.Drawing.Size(221, 23);
-
+            this.textBox2.Size = new System.Drawing.Size(273, 31);
             this.textBox2.TabIndex = 41;
             // 
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(381, 55);
             this.textBox1.Name = "textBox1";
-
-            this.textBox1.Size = new System.Drawing.Size(330, 31);
-
-            this.textBox1.Size = new System.Drawing.Size(221, 23);
-
+            this.textBox1.Size = new System.Drawing.Size(273, 31);
             this.textBox1.TabIndex = 39;
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-
-            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label8.ForeColor = System.Drawing.Color.DarkRed;
-            this.label8.Location = new System.Drawing.Point(218, 390);
-
             this.label8.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label8.ForeColor = System.Drawing.Color.Gray;
-            this.label8.Location = new System.Drawing.Point(201, 279);
+            this.label8.Location = new System.Drawing.Point(128, 394);
             this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(107, 29);
+            this.label8.Size = new System.Drawing.Size(70, 21);
             this.label8.TabIndex = 38;
             this.label8.Text = "Tiền Mặt";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label5.ForeColor = System.Drawing.Color.Coral;
-            this.label5.Location = new System.Drawing.Point(115, 361);
-
             this.label5.ForeColor = System.Drawing.Color.Black;
-            this.label5.Location = new System.Drawing.Point(200, 263);
+            this.label5.Location = new System.Drawing.Point(127, 365);
             this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(216, 29);
             this.label5.TabIndex = 37;
@@ -177,15 +142,10 @@
             // label6
             // 
             this.label6.AutoSize = true;
-
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label6.ForeColor = System.Drawing.Color.Coral;
-            this.label6.Location = new System.Drawing.Point(159, 297);
-
             this.label6.ForeColor = System.Drawing.Color.Black;
-            this.label6.Location = new System.Drawing.Point(200, 225);
+            this.label6.Location = new System.Drawing.Point(171, 297);
             this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(172, 29);
             this.label6.TabIndex = 36;
@@ -194,15 +154,10 @@
             // label7
             // 
             this.label7.AutoSize = true;
-
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label7.ForeColor = System.Drawing.Color.Coral;
-            this.label7.Location = new System.Drawing.Point(262, 235);
-
             this.label7.ForeColor = System.Drawing.Color.Black;
-            this.label7.Location = new System.Drawing.Point(201, 187);
+            this.label7.Location = new System.Drawing.Point(274, 237);
             this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(69, 29);
             this.label7.TabIndex = 34;
@@ -211,15 +166,10 @@
             // label4
             // 
             this.label4.AutoSize = true;
-
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label4.ForeColor = System.Drawing.Color.Coral;
-            this.label4.Location = new System.Drawing.Point(218, 177);
-
             this.label4.ForeColor = System.Drawing.Color.Black;
-            this.label4.Location = new System.Drawing.Point(201, 149);
+            this.label4.Location = new System.Drawing.Point(230, 173);
             this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(113, 29);
             this.label4.TabIndex = 35;
@@ -228,15 +178,10 @@
             // label2
             // 
             this.label2.AutoSize = true;
-
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label2.ForeColor = System.Drawing.Color.Coral;
-            this.label2.Location = new System.Drawing.Point(143, 121);
-
             this.label2.ForeColor = System.Drawing.Color.Black;
-            this.label2.Location = new System.Drawing.Point(200, 113);
+            this.label2.Location = new System.Drawing.Point(155, 110);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(188, 29);
             this.label2.TabIndex = 33;
@@ -245,15 +190,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
-
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.ForeColor = System.Drawing.Color.Coral;
-            this.label1.Location = new System.Drawing.Point(89, 55);
-
             this.label1.ForeColor = System.Drawing.Color.Black;
-            this.label1.Location = new System.Drawing.Point(200, 77);
+            this.label1.Location = new System.Drawing.Point(101, 55);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(242, 29);
             this.label1.TabIndex = 32;
@@ -263,13 +203,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-
-            this.BackColor = System.Drawing.Color.NavajoWhite;
-            this.ClientSize = new System.Drawing.Size(832, 623);
-
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
-            this.ClientSize = new System.Drawing.Size(800, 450);
-
+            this.ClientSize = new System.Drawing.Size(800, 602);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox6);
@@ -285,13 +220,10 @@
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-
             this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-
-
             this.Name = "Frm_HoaDon";
-            this.Text = "Frm_Receipt";
+            this.Text = "Frm_Hoadon";
             this.TransparencyKey = System.Drawing.Color.PaleTurquoise;
             this.Load += new System.EventHandler(this.Frm_Receipt_Load);
             this.ResumeLayout(false);
