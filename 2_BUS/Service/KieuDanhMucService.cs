5880a07b145ca843208f4247db6e53cea5923637
dung <dungtqph28227@fpt.edu.vn>
2022-11-18 13:28:55 +0700

    dung

---
+++ 2_BUS/Service/KieuDanhMucService.cs
@@ -0,0 +1,37 @@
+﻿using _1.DAL.IRepostiories;
+using _1.DAL.Repostiores;
+using _1_DAL.IRepostiories;
+using _1_DAL.Repostiores;
+using _2_BUS.IService;
+using _2_BUS.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_BUS.Service
+{
+    public class KieuDanhMucService : IKieuDanhMucServices
+    {
+        private ITypeTableOfContents _kieuDanhMuc;
+        private List<KieuDanhMucViewModels> KieuDanhMucList;
+        public KieuDanhMucService()
+        {
+            _kieuDanhMuc = new TypeTableOfContentRepositores();
+        }
+        public List<KieuDanhMucViewModels> GetallKieuDM()
+        {
+            KieuDanhMucList =
+                (from a in _kieuDanhMuc.getAll()
+                 select new KieuDanhMucViewModels()
+                 {
+                     IdDanhMuc = a.IdDanhMuc,
+                     TenKieuDanhMuc = a.TenKieuDanhMuc,
+                     IdHangHoa = a.IdHangHoa,
+
+                 }).ToList();
+            return KieuDanhMucList;
+        }
+    }
+}
