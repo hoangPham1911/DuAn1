3ba63e6b1bcf9a52cf665f98be67ad0a97209614
dung <dungtqph28227@fpt.edu.vn>
2022-12-09 12:31:13 +0700

    dung

--- 3_PL/View/Frm_NguoiDung.Designer.cs
+++ 3_PL/View/Frm_NguoiDung.Designer.cs
@@ -330,8 +330,6 @@
             this.label3.Text = "Avartar người dùng";
             // 
             // btnGiaoCa
-<<<<<<< HEAD
-=======
             // 
             this.btnGiaoCa.BackColor = System.Drawing.Color.LightSalmon;
             this.btnGiaoCa.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
@@ -347,7 +345,6 @@
             this.btnGiaoCa.Click += new System.EventHandler(this.btnGiaoCa_Click);
             // 
             // Frm_NguoiDung
->>>>>>> 1214351149425e077e21436042785bbfdb9626eb
             // 
             this.btnGiaoCa.BackColor = System.Drawing.Color.LightSalmon;
             this.btnGiaoCa.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
@@ -365,11 +362,11 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.PeachPuff;
-<<<<<<< HEAD
+
             this.ClientSize = new System.Drawing.Size(857, 558);
-=======
+
             this.ClientSize = new System.Drawing.Size(943, 483);
->>>>>>> 1214351149425e077e21436042785bbfdb9626eb
+
             this.Controls.Add(this.btnGiaoCa);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.ptb_anh);
@@ -392,10 +389,9 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.label12);
             this.Controls.Add(this.panel1);
-<<<<<<< HEAD
+
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-=======
->>>>>>> 1214351149425e077e21436042785bbfdb9626eb
+
             this.Name = "Frm_NguoiDung";
             this.Text = "Frm_ThongTinNguoiDung";
             this.Load += new System.EventHandler(this.Frm_ThongTinNguoiDung_Load);
@@ -430,11 +426,10 @@
         private Label lb_maNguoid;
         private PictureBox ptb_anh;
         private Label label3;
-<<<<<<< HEAD
+
         private Label lb_tien;
         private Label label4;
-=======
->>>>>>> 1214351149425e077e21436042785bbfdb9626eb
+
         private Button btnGiaoCa;
     }
 }
